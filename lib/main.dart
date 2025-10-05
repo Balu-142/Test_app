c5056e327a8c1ad72a050723af4aad2a43f20ccf
Balu Pamba <115220302+Balu-142@users.noreply.github.com>
2025-10-05 10:06:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,28 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:http/http.dart' as http;
-//
-// import 'chat_App/core/utils/network_info.dart';
-// import 'chat_App/data/datasources/auth_remote_data_source.dart';
-// import 'chat_App/data/repositories/auth_repository_impl.dart';
-// import 'chat_App/domain/usecases/login_usecase.dart';
-// import 'chat_App/presentation/blocs/auth_bloc.dart';
-// import 'chat_App/presentation/pages/login_page.dart';
-//
-// // import 'core/utils/network_info.dart';
-// // import 'data/datasources/auth_remote_data_source.dart';
-// // import 'data/repositories/auth_repository_impl.dart';
-// // import 'domain/usecases/login_usecase.dart';
-// // import 'presentation/blocs/auth_bloc.dart';
-// // import 'presentation/pages/login_page.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   final networkInfo = NetworkInfo();
-//   final httpClient = http.Client();
-//
-//   MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final remoteDataSource = AuthRemoteDataSourceImpl(client: httpClient);
-//     final repository = AuthRepositoryImpl(
-//       remoteDataSource: remoteDataSource,
-//       networkInfo: networkInfo,
-//     );
-//     final loginUseCase = LoginUseCase(repository);
-//
-//     return MaterialApp(
-//       title: 'Clean Architecture Login',
-//       debugShowCheckedModeBanner: false,
-//       home: BlocProvider(
-//         create: (_) => AuthBloc(loginUseCase: loginUseCase),
-//         child: const LoginPage(),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 
 import 'chat_App/core/utils/network_info.dart';
 
-// Data Sources
 import 'chat_App/data/datasources/auth_remote_data_source.dart';
 import 'chat_App/data/datasources/chat_remote_data_source.dart';
 import 'chat_App/data/datasources/message_remote_data_source.dart';
 
-// Repositories
 import 'chat_App/data/repositories/auth_repository_impl.dart';
 import 'chat_App/data/repositories/chat_repository_impl.dart';
 import 'chat_App/data/repositories/message_repository_impl.dart';
 
-// Domain Repositories
 import 'chat_App/domain/repositories/auth_repository.dart';
 import 'chat_App/domain/repositories/chat_repository.dart';
 import 'chat_App/domain/repositories/message_repository.dart';
 
-// Use Cases
 import 'chat_App/domain/usecases/login_usecase.dart';
 import 'chat_App/domain/usecases/get_chats_usecase.dart';
 import 'chat_App/domain/usecases/get_messages_usecase.dart';
 import 'chat_App/domain/usecases/send_message_usecase.dart';
 
-// BLoCs
 import 'chat_App/presentation/blocs/auth_bloc.dart';
 
-// Pages
 import 'chat_App/presentation/pages/login_page.dart';
 
 void main() {
